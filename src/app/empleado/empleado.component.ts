31f3d33d7f696c7ed053a7fb1547b9394ec87e95
Michelle-RA <michelleramirezagudelo@gmail.com>
2023-08-28 22:23:45 -0500

    se hace un nuevo ejercicio

--- src/app/empleado/empleado.component.ts
+++ src/app/empleado/empleado.component.ts
@@ -6,8 +6,31 @@ import { Component, OnInit } from '@angular/core';
   //template:"<p>aqui va un empleado!</p>",
   styleUrls: ['./empleado.component.css']
   //styles:["p{background-color:red;}"]
+
 })
 export class EmpleadoComponent implements OnInit{
+
+  nombre="juan";
+  apellido="diaz";
+  //private edad=18;
+  edad=18;
+  //empresa="seti";
+
+  /*getEdad(){
+    return this.edad;
+  }*/
+
+  /*llamaEmpresa(value:String){
+
+  }*/
+
+  habilitacionCuadro=true;
+
+  usuarioRegistrado=false;
+
+  GetRegistroUsuario(){
+    this.usuarioRegistrado=false;
+  }
 
   constructor(){}
 
