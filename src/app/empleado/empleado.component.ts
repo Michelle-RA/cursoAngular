ff3ad93296165c574dbe7f6a835d16fd8fca1210
Michelle-RA <michelleramirezagudelo@gmail.com>
2023-08-29 21:45:17 -0500

    Biding Bidireccional

--- src/app/empleado/empleado.component.ts
+++ src/app/empleado/empleado.component.ts
@@ -14,7 +14,11 @@ export class EmpleadoComponent implements OnInit{
   apellido="diaz";
   //private edad=18;
   edad=18;
-  //empresa="seti";
+  empresa="google";
+
+  cambioEmpresa(event:Event){
+    this.empresa=(<HTMLInputElement>event.target).value;
+  }
 
   /*getEdad(){
     return this.edad;
@@ -24,12 +28,27 @@ export class EmpleadoComponent implements OnInit{
 
   }*/
 
-  habilitacionCuadro=true;
+  habilitacionCuadro=false;
 
   usuarioRegistrado=false;
 
+  textoDeRegistro="No hay nadie registrado";
+
   GetRegistroUsuario(){
     this.usuarioRegistrado=false;
+  }
+
+  setUsuarioRegistrado(event:Event){
+    //alert ("el usuario se ha registrado");
+    //this.textoDeRegistro="El usuario ya se registro";
+
+    if((<HTMLInputElement>event.target).value=="si"){
+
+      this.textoDeRegistro="El usuario se acaba de registrar";
+
+    }else{
+      this.textoDeRegistro="No hay nadie registrado";
+    }
   }
 
   constructor(){}
